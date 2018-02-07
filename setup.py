90384cb0cd349af9e1914de2e7291e12b33963c4
Daniel Jensen <jensendw@gmail.com>
2018-02-07 10:22:16 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 
 setup(name='vault-gatekeeper-client',
-      version='0.0.6',
+      version='0.0.7',
       description='vault-gatekeeper-client for interacting with vault-gatekeeper-mesos service',
       url='https://github.com/jensendw/vault-gatekeeper-client',
       author='Daniel Jensen',
